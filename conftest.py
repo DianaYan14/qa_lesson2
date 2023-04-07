bc02575e679c7dc9d9b56c78ee0c98ad3145e209
Diana Yanysheva <dintos.pisos@gmail.com>
2023-04-07 18:30:11 +0500

    выставлен определенный размер окна

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from selene import browser
 @pytest.fixture(scope="session")
 def browser_go():
     browser.open('https://google.com')
-    browser.driver.maximize_window()
+    browser.driver.set_window_size(1920, 1080)
     # открытие браузера
     yield
     # закрытие браузера
